b0d9292ae2437507cfc616dd5a5b19ff29b92d03
zhimmat <zeyad.himmat@live.com>
2016-03-23 17:21:32 -0400

    added rest of code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 var $input = $('#to-do-input');
 var $list = $('.list');
+
+$('form').on('submit', function (e){
+  var $li = $('<li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+  $input.val(' ');
+});
+
+$('li').on('click', 'li', function (e){
+  $(this).toggleClass('line');
+});
